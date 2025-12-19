d46e4bb89c208995205b754f8a0084b07fe39481
Brandon Arrendondo <brandon.arrendondo@bissell.com>
2025-12-19 18:53:48 -0500

    Bump version to 1.0.18 (versionCode 21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.srg.inventory"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.0.17"
+        versionCode = 21
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
